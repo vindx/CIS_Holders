55d439a2ad186df0e76cc68a7c9ec09921d23238
Kiryl Karelin <kiryl.karelin4@innowise-group.com>
2022-12-22 15:00:41 +0200

    added-splash-screen

--- App.tsx
+++ App.tsx
@@ -1,16 +1,23 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Provider } from 'react-redux'
+import RNBootSplash from 'react-native-bootsplash'
 
 import Router from '~router'
 import store from '~store'
 import { ServiceModalProvider } from '~context/ServiceModal'
 
-const App = () => (
-  <Provider store={store}>
-    <ServiceModalProvider>
-      <Router />
-    </ServiceModalProvider>
-  </Provider>
-)
+const App = () => {
+  useEffect(() => {
+    RNBootSplash.hide()
+  }, [])
+
+  return (
+    <Provider store={store}>
+      <ServiceModalProvider>
+        <Router />
+      </ServiceModalProvider>
+    </Provider>
+  )
+}
 
 export default App
--- android/app/src/main/java/com/cis_holders/MainActivity.java
+++ android/app/src/main/java/com/cis_holders/MainActivity.java
@@ -1,6 +1,7 @@
 package com.cis_holders;
 
 import android.os.Bundle;
+import com.zoontek.rnbootsplash.RNBootSplash;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -18,6 +19,7 @@ public class MainActivity extends ReactActivity {
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
+    RNBootSplash.init(this);
     super.onCreate(null);
   }
 
