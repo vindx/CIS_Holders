7ab38fdefda54c104bc1d1707dca6bf27b3ae669
Kiryl Karelin <kiryl.karelin4@innowise-group.com>
2023-01-19 16:19:22 +0200

    added-icons-constants

---
+++ src/constants/iconNames.ts
@@ -0,0 +1,2 @@
+export const ARROW_DOWN_ICON = 'arrow-down-outline'
+export const ARROW_UP_ICON = 'arrow-up-outline'
--- src/router/BottomTabNavigator/types.ts
+++ src/router/BottomTabNavigator/types.ts
@@ -1,12 +1,13 @@
 import { BottomTabHeaderProps } from '@react-navigation/bottom-tabs'
 import { TSortingName } from '~constants/firestore'
+import { ARROW_DOWN_ICON, ARROW_UP_ICON } from '~constants/iconNames'
 
 export interface ICustomHeaderProps extends BottomTabHeaderProps {
   toggleDrawer: () => void
 }
 
 export interface ISortItemProps {
-  iconName: 'arrow-down-outline' | 'arrow-up-outline'
+  iconName: typeof ARROW_DOWN_ICON | typeof ARROW_UP_ICON
   onPress: () => void
   text: TSortingName
 }
